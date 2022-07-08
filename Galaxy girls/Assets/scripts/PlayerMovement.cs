c917bf400f2c9448716305701b908b2331a34cbd
tycelm <70135829+tycelm@users.noreply.github.com>
2022-07-08 16:33:36 +0800

    What I've done so far

---
+++ Galaxy girls/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public PauseMenu menu;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    public Animator animator;
+    bool paused;
+    bool jump = false;
+
+        // Update is called once per frame
+        void Update()
+    {
+        paused = menu.GameIsPaused;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if(!paused)
+        {
+            
+            animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+        }
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
---
+++ Galaxy girls/Assets/scripts/SameLevelDoor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SameLevelDoor : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject target;
+    public bool wentIn;
+    [SerializeField] private Image customImage;
+    public Animator anim;
+    public Image blac;
+    
+
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if(col.CompareTag("Player"))
+            {
+                customImage.enabled = true;
+                if(Input.GetKeyDown("space"))
+                {
+                    StartCoroutine("Transport");
+                }
+            }
+        }
+
+        void OnTriggerStay2D(Collider2D col)
+        {
+            if(col.CompareTag("Player"))
+            {
+                if(Input.GetKeyDown("space"))
+                {
+                    StartCoroutine("Transport");
+                }
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D col)
+        {
+            if(col.CompareTag("Player"))
+            {
+                customImage.enabled = false;
+            }
+        }
+
+    IEnumerator Transport()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => blac.color.a == 1);
+        player.transform.position = target.transform.position;
+        anim.SetBool("Fade", false);
+    }
+    
+}
