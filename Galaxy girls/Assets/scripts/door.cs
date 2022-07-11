b5a5fb1938727ad9002534fcd0686ea8c36be329
tycelm <70135829+tycelm@users.noreply.github.com>
2022-07-11 23:38:45 +0800

    did idle animation (fix position when i wake up

---
+++ Galaxy girls/Assets/scripts/door.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class door : MonoBehaviour
+{
+    [SerializeField] private Image customImage;
+    public Animator anim;
+    public Image blac;
+    public int scene;
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            customImage.enabled = true;
+            if (Input.GetButtonDown("Space"))
+            {
+                StartCoroutine("Leave");
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (Input.GetButtonDown("Space"))
+            {
+                StartCoroutine("Leave");
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            customImage.enabled = false;
+        }
+    }
+
+    IEnumerator Leave()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => blac.color.a == 1);
+        SceneManager.LoadScene(scene);
+    }
+
+}
