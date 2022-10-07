ac442cb3e3d6ad37d8decd0056e30ddb1585eaf6
tycelm <70135829+tycelm@users.noreply.github.com>
2022-10-07 18:10:04 -0400

    Implemented last bullet and dying
    dying not properly done yet

---
+++ Galaxy girls/Assets/bounce.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D m_Rigidbody2D;
+    public int x;
+    public float posx;
+    public float posy;
+    public float posz;
+    public int jump;
+
+    void OnEnable()
+    {
+        transform.position = new Vector3(posx, posy, posz);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * x * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("bouncedry"))
+        {
+            m_Rigidbody2D.AddForce(new Vector2(0f, jump));
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("bouncedry"))
+        {
+            m_Rigidbody2D.AddForce(new Vector2(0f, jump));
+        }
+    }
+}
---
+++ Galaxy girls/Assets/stats.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class stats : MonoBehaviour
+{
+    public int lives = 3;
+    public int scene;
+    private bool grace;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("hurty"))
+        {
+            if (!grace)
+            {
+                Debug.Log("Owie!");
+                Debug.Log(lives);
+                lives -= 1;
+                if (lives == 0)
+                {
+                    // to be fully implemented ltr
+                    SceneManager.LoadScene(scene);
+                }
+                grace = true;
+                StartCoroutine("Grace");
+            }
+        }
+    }
+
+    IEnumerator Grace()
+    {
+        yield return new WaitForSeconds(2);
+        grace = false;
+    }
+}
