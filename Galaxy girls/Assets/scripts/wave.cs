c9f3dcb3f2fd85700a90220965e2a9cbe588c431
tycelm <70135829+tycelm@users.noreply.github.com>
2022-10-25 00:05:52 -0400

    Scripted like 20 seconds of the game :p

--- Galaxy girls/Assets/scripts/wave.cs
+++ Galaxy girls/Assets/scripts/wave.cs
@@ -6,20 +6,23 @@ public class wave : MonoBehaviour
 {
     public GameObject left;
     public GameObject right;
-    public GameObject original;
-    public GameObject drop;
-    private float sec = 0.5f;
+    public float posx;
+    public float posy;
+    public float posz;
+    private float sec = 0.4f;
 
     void OnEnable()
     {
+        left.transform.position = new Vector3(posx, posy, posz);
+        right.transform.position = new Vector3(posx, posy, posz);
         StartCoroutine("Leave");
     }
 
     // Update is called once per frame
     void Update()
     {
-        left.transform.Translate(Vector3.left * 17 * Time.deltaTime);
-        right.transform.Translate(Vector3.right * 10 * Time.deltaTime);
+        left.transform.Translate(Vector3.left * 20 * Time.deltaTime);
+        right.transform.Translate(Vector3.right * 20 * Time.deltaTime);
     }
 
     IEnumerator Leave()
