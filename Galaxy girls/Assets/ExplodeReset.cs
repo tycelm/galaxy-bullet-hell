96e4e52829c2c3d9d726101a09e18094c113d63e
tycelm <70135829+tycelm@users.noreply.github.com>
2022-11-01 00:03:04 -0400

    scripted a bit more and fixed drop explode, gn

---
+++ Galaxy girls/Assets/ExplodeReset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeReset : MonoBehaviour
+{
+    public float posx;
+    public float posy;
+    public float posz;
+    void OnEnable()
+    {
+        transform.position = new Vector3(posx, posy, posz);
+    }
+}
